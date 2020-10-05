c05f39f29b8d33e53a9b2a2235cd0944b7a2962a
Shuvo <meghsohor@yahoo.com>
2020-10-06 00:00:58 +0600

    Updates

---
+++ precache-manifest.ad44c412b7b3fa209fc35acc9c0d7612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a859904ff62e52c4482e30eeab07dc7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e644e5529320708b838",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "3e644e5529320708b838",
+    "url": "/static/js/2.597785a8.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.597785a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9aba555d4330ab7cab1",
+    "url": "/static/js/main.00e23624.chunk.js"
+  },
+  {
+    "revision": "aeea36bdadbfd99c7e94",
+    "url": "/static/js/runtime-main.f7969a42.js"
+  }
+]);
